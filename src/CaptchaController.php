6afab47a00a318af139a533db8f8281b8a511ff1
Christian Bayer <christian.bayer@universo.univates.br>
2017-06-14 22:14:56 -0300

    Initial commit

---
+++ src/CaptchaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Xablau\Captcha;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CaptchaController extends Controller
+{
+
+}
---
+++ src/CaptchaServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Xablau\Captcha;
+
+use Illuminate\Support\ServiceProvider;
+
+class CaptchaServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
