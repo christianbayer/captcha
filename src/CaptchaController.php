42a5ef840b683b859914e588faf39d4fc17f5e3f
Christian Bayer <christian.bayer@universo.univates.br>
2017-06-14 22:31:42 -0300

    Ajuste composer.json

--- src/CaptchaController.php
+++ src/CaptchaController.php
@@ -4,8 +4,25 @@ namespace Xablau\Captcha;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class CaptchaController extends Controller
 {
 
+    public function __construct()
+    {
+    }
+
+    public function create()
+    {
+        $canvasHeigth = 70;
+        $canvasWidth = 200;
+
+
+        $img = Image::canvas(32, 32, '#ff0000');
+
+        $img->save(public_path('captcha/'.time().'.png'));
+
+        dd(1);
+    }
 }
